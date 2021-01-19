44c272898b879851cfa4e7c9d90f349b015be7f7
Shin Hiroe <hiroe.orz@gmail.com>
2021-01-19 14:16:50 +0900

    change ENTER <-> LANG1

--- keyboards/mint60/keymaps/custom_mac/keymap.c
+++ keyboards/mint60/keymaps/custom_mac/keymap.c
@@ -13,6 +13,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/*
+  BUILD: % make mint60:custom_mac
+  WRITE: % make mint60:custom_mac:avrdude
+ */
+
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes {
@@ -25,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
     KC_LCTL,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,   \
     KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,   KC_UP,  MO(1), \
-    KC_ESC,    KC_LALT,    GUI_T(KC_LANG2),  KC_LANG2,    KC_SPC,       KC_LANG1, KC_ENT,  KC_BSPC,        KC_LEFT,KC_DOWN,KC_RGHT \
+    KC_ESC,    KC_LALT,    GUI_T(KC_LANG2),  KC_LANG2,    KC_SPC,       KC_ENT,   KC_LANG1, KC_BSPC,        KC_LEFT,KC_DOWN,KC_RGHT \
   ),
   [1] = LAYOUT( \
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL, \
